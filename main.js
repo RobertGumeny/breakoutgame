497efd85142548030f97ec7de37c802083d5f4ea
Robert Gumeny <robert.gumeny528@gmail.com>
2020-06-03 23:02:25 -0600

    ball, paddle, bricks are drawn to screen

--- main.js
+++ main.js
@@ -2,7 +2,107 @@ const rulesBtn = document.getElementById('rules-btn')
 const closeBtn = document.getElementById('close-btn')
 const rules = document.getElementById('rules')
 const canvas = document.getElementById('canvas')
+/** @type {CanvasRenderingContext2D} */
+const ctx = canvas.getContext('2d')
 
+let score = 0;
+
+const brickRowCount = 9;
+const brickColCount = 6;
+
+
+// NOTE Create ball properties
+
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2 + 20,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+}
+
+// NOTE Create paddle properties
+
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0
+}
+
+// NOTE Create brick properties
+
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true
+}
+
+// NOTE Create bricks
+
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
+// NOTE Draw ball to canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = '#1d2d44';
+  ctx.fill();
+  ctx.closePath();
+}
+
+// NOTE Draw paddle to canvas
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = '#1d2d44';
+  ctx.fill();
+  ctx.closePath();
+}
+
+// NOTE Draw bricks on canvas
+function drawBricks() {
+  bricks.forEach(column => {
+    column.forEach(brick => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? '#1d2d44' : 'transparent';
+      ctx.fill();
+      ctx.closePath;
+    })
+  })
+}
+// NOTE Draw score
+
+function drawScore() {
+  ctx.font = '20px Ubuntu';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 35)
+}
+
+// NOTE Draw everything to canvas
+function draw() {
+  drawBall()
+  drawPaddle()
+  drawScore()
+  drawBricks();
+}
+
+
+draw()
 // NOTE Rules show/hide event listeners
 
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
